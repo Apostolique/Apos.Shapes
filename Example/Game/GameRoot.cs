355f8f7f538b120e76c243552a18be746070b6bd
Jean-David Moisan <vynecompiler@gmail.com>
2021-07-07 17:04:42 -0400

    Add camera with zoom

--- Example/Game/GameRoot.cs
+++ Example/Game/GameRoot.cs
@@ -1,8 +1,10 @@
+using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Apos.Input;
 using Apos.Shapes;
+using Apos.Camera;
 
 namespace GameProject {
     public class GameRoot : Game {
@@ -24,6 +26,9 @@ namespace GameProject {
             _sb = new ShapeBatch(GraphicsDevice, Content.Load<Effect>("Shapes"));
 
             InputHelper.Setup(this);
+
+            IVirtualViewport defaultViewport = new DefaultViewport(GraphicsDevice, Window);
+            _camera = new Camera(defaultViewport);
         }
 
         protected override void Update(GameTime gameTime) {
@@ -31,6 +36,10 @@ namespace GameProject {
 
             if (_quit.Pressed())
                 Exit();
+            UpdateCameraInput();
+
+            _camera.Z = _camera.ScaleToZ(ExpToScale(Interpolate(ScaleToExp(_camera.ZToScale(_camera.Z, 0f)), _targetExp, _speed, _snapDistance)), 0f);
+            _camera.Rotation = Interpolate(_camera.Rotation, _targetRotation, _speed, _snapDistance);
 
             InputHelper.UpdateCleanup();
             base.Update(gameTime);
@@ -39,18 +48,82 @@ namespace GameProject {
         protected override void Draw(GameTime gameTime) {
             GraphicsDevice.Clear(Color.Black);
 
-            _sb.Begin(Matrix.CreateTranslation(0f, 0f, 0f) * Matrix.CreateScale(1f, 1f, 1f));
+            _sb.Begin(_camera.View);
+            _sb.DrawCircle(new Vector2(100f, 0f), 100f, Color.Red, Color.White, 4f);
             _sb.DrawCircle(new Vector2(100, 100), 50, Color.Red, Color.White, 4f);
             _sb.DrawCircle(new Vector2(170, 150), 75, Color.Blue * 0.5f, Color.White, 2f);
             _sb.DrawCircle(new Vector2(400, 120), 100, Color.Green, Color.White, 10f);
             _sb.End();
 
-            _sb.Begin(Matrix.CreateTranslation(0f, 0f, 0f) * Matrix.CreateScale(3f, 1f, 1f));
-            _sb.DrawCircle(new Vector2(100, 300), 50, Color.Red, Color.White, 4f);
-            _sb.DrawCircle(new Vector2(100, 300), 20, Color.Black * 0.7f, Color.Black, 4f);
-            _sb.End();
-
             base.Draw(gameTime);
+        }
+
+        private void UpdateCameraInput() {
+            int x = InputHelper.NewMouse.X;
+            int y = InputHelper.NewMouse.Y;
+
+            if (MouseCondition.Scrolled()) {
+                int scrollDelta = MouseCondition.ScrollDelta;
+                _targetExp = MathHelper.Clamp(_targetExp - scrollDelta * _expDistance, _maxExp, _minExp);
+            }
+
+            if (RotateLeft.Pressed()) {
+                _targetRotation += MathHelper.PiOver4;
+            }
+            if (RotateRight.Pressed()) {
+                _targetRotation -= MathHelper.PiOver4;
+            }
+
+            _mouseWorld = _camera.ScreenToWorld(x, y);
+
+            if (CameraDrag.Pressed()) {
+                _dragAnchor = _mouseWorld;
+                _isDragged = true;
+            }
+            if (_isDragged && CameraDrag.HeldOnly()) {
+                _camera.XY += _dragAnchor - _mouseWorld;
+                _mouseWorld = _dragAnchor;
+            }
+            if (_isDragged && CameraDrag.Released()) {
+                _isDragged = false;
+            }
+
+            if (CameraReset.Pressed()) {
+                _camera.XY = Vector2.Zero;
+                _camera.Rotation = 0f;
+                _camera.Scale = Vector2.One;
+                _camera.FocalLength = 1f;
+            }
+        }
+        private float ScaleToExp(float scale) {
+            return -MathF.Log(scale);
+        }
+        private float ExpToScale(float exp) {
+            return MathF.Exp(-exp);
+        }
+
+        /// <summary>
+        /// Poor man's tweening function.
+        /// If the result is stored in the `from` value, it will create a nice interpolation over multiple frames.
+        /// </summary>
+        /// <param name="from">The value to start from.</param>
+        /// <param name="target">The value to reach.</param>
+        /// <param name="speed">A value between 0f and 1f.</param>
+        /// <param name="snapNear">When the difference between the target and the result is smaller than this value, the target will be returned.</param>
+        private static float Interpolate(float from, float target, float speed, float snapNear) {
+            float result = MathHelper.Lerp(from, target, speed);
+
+            if (from < target) {
+                result = MathHelper.Clamp(result, from, target);
+            } else {
+                result = MathHelper.Clamp(result, target, from);
+            }
+
+            if (MathF.Abs(target - result) < snapNear) {
+                return target;
+            } else {
+                return result;
+            }
         }
 
         GraphicsDeviceManager _graphics;
@@ -62,5 +135,25 @@ namespace GameProject {
                 new KeyboardCondition(Keys.Escape),
                 new GamePadCondition(GamePadButton.Back, 0)
             );
+        ICondition RotateLeft = new KeyboardCondition(Keys.OemComma);
+        ICondition RotateRight = new KeyboardCondition(Keys.OemPeriod);
+
+        ICondition CameraDrag = new MouseCondition(MouseButton.MiddleButton);
+
+        ICondition CameraReset = new KeyboardCondition(Keys.R);
+
+        Camera _camera;
+        Vector2 _mouseWorld = Vector2.Zero;
+        Vector2 _dragAnchor = Vector2.Zero;
+        bool _isDragged = false;
+
+        float _targetExp = 0f;
+        float _targetRotation = 0f;
+        float _speed = 0.08f;
+        float _snapDistance = 0.001f;
+
+        float _expDistance = 0.002f;
+        float _maxExp = -5f;
+        float _minExp = 2f;
     }
 }
