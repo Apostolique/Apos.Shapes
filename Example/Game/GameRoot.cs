69e2dc9cc300aaf95ab062e20746b2b1f2bf34f2
Jean-David Moisan <vynecompiler@gmail.com>
2021-08-17 01:01:00 -0400

    Update example

--- Example/Game/GameRoot.cs
+++ Example/Game/GameRoot.cs
@@ -33,6 +33,8 @@ namespace GameProject {
 
             _fontSystem = new FontSystem();
             _fontSystem.AddFont(TitleContainer.OpenStream($"{Content.RootDirectory}/source-code-pro-medium.ttf"));
+
+            _200x200 = Content.Load<Texture2D>("200x200");
         }
 
         protected override void Update(GameTime gameTime) {
@@ -52,19 +54,76 @@ namespace GameProject {
             base.Update(gameTime);
         }
 
+        Color[] _c1 = new Color[] {
+            new Color(239, 246, 255),
+            new Color(219, 234, 254),
+            new Color(191, 219, 254),
+            new Color(147, 197, 253),
+            new Color(96, 165, 250),
+            new Color(59, 130, 246),
+            new Color(37, 99, 235),
+            new Color(29, 78, 216),
+            new Color(30, 64, 175),
+            new Color(30, 58, 138),
+        };
+        Color[] _c2 = new Color[] {
+            new Color(254, 242, 242),
+            new Color(254, 226, 226),
+            new Color(254, 202, 202),
+            new Color(252, 165, 165),
+            new Color(248, 113, 113),
+            new Color(239, 68, 68),
+            new Color(220, 38, 38),
+            new Color(185, 28, 28),
+            new Color(153, 27, 27),
+            new Color(127, 29, 29),
+        };
+        Color[] _c3 = new Color[] {
+            new Color(236, 253, 245),
+            new Color(209, 250, 229),
+            new Color(167, 243, 208),
+            new Color(110, 231, 183),
+            new Color(52, 211, 153),
+            new Color(16, 185, 129),
+            new Color(5, 150, 105),
+            new Color(4, 120, 87),
+            new Color(6, 95, 70),
+            new Color(6, 78, 59),
+        };
+
         protected override void Draw(GameTime gameTime) {
             _fps.Draw(gameTime);
             GraphicsDevice.Clear(Color.Black);
 
-            _sb.Begin(_camera.View);
-            _sb.FillCircle(new Vector2(200f, -100f), 100f, Color.Red, Color.White, 4f * _camera.WorldToScreenScale());
-            _sb.FillCircle(new Vector2(200, 0), 50, Color.Purple, Color.White, 0f);
-            _sb.FillCircle(new Vector2(270, 50), 75, Color.Blue * 0.5f, Color.White, 2f);
-            _sb.FillCircle(new Vector2(80, 120), 100, Color.Green, Color.White, 10f);
+            var mouse = InputHelper.NewMouse.Position.ToVector2();
+            var a = new Vector2(0, 0);
+            var b = _camera.ScreenToWorld(mouse);
+            // var b = new Vector2(100, 0);
 
-            _sb.FillRectangle(new Vector2(-200, -200), new Vector2(100, 100), Color.Green, Color.White, 0f);
-            _sb.FillRectangle(new Vector2(-200, -50), new Vector2(200, 100), Color.Green, Color.White, 2f);
-            _sb.FillRectangle(new Vector2(-350, -50), new Vector2(100, 200), Color.Blue, Color.White, 10f);
+            // var c = Slide(a, b);
+            // var d = Slide(b, a);
+
+
+            // _s.Begin(transformMatrix: _camera.View);
+            // _s.Draw(_200x200, a - new Vector2(100, 100), Color.White);
+            // _s.End();
+
+            _sb.Begin(_camera.View);
+            _sb.BorderLine(new Vector2(100, 20), new Vector2(450, -15), 20, Color.White, 2f);
+
+            _sb.DrawCircle(new Vector2(120, 120), 75, new Color(96, 165, 250), new Color(191, 219, 254), 4f);
+            _sb.DrawCircle(new Vector2(120, 120), 30, Color.White, Color.Black, 20f);
+
+            _sb.DrawCircle(new Vector2(370, 120), 100, new Color(96, 165, 250), new Color(191, 219, 254), 4f);
+            _sb.DrawCircle(new Vector2(370, 120), 40, Color.White, Color.Black, 20f);
+
+            _sb.DrawCircle(new Vector2(190, 270), 10, Color.Black, Color.White, 2f);
+            _sb.DrawCircle(new Vector2(220, 270), 10, Color.Black, Color.White, 2f);
+
+            _sb.FillCircle(new Vector2(235, 400), 30, new Color(220, 38, 38));
+            _sb.FillRectangle(new Vector2(235, 370), new Vector2(135, 60), new Color(220, 38, 38));
+            _sb.FillCircle(new Vector2(235, 400), 20, Color.White);
+            _sb.FillRectangle(new Vector2(235, 380), new Vector2(125, 40), Color.White);
             _sb.End();
 
             var font = _fontSystem.GetFont(24);
@@ -73,6 +132,19 @@ namespace GameProject {
             _s.End();
 
             base.Draw(gameTime);
+        }
+
+        private Vector2 Slide(Vector2 a, Vector2 b) {
+            var c = Vector2.Normalize(b - a) * 10f;
+            return b + c;
+        }
+        private Vector2 Clockwise(Vector2 a, Vector2 b) {
+            var c = Vector2.Normalize(b - a) * 10f;
+            return new Vector2(c.Y, -c.X) + a;
+        }
+        private Vector2 CounterClockwise(Vector2 a, Vector2 b) {
+            var c = Vector2.Normalize(b - a) * 10f;
+            return new Vector2(-c.Y, c.X) + a;
         }
 
         private void UpdateCameraInput() {
@@ -176,6 +248,8 @@ namespace GameProject {
 
         float _expDistance = 0.002f;
         float _maxExp = -5f;
-        float _minExp = 2f;
+        float _minExp = 5f;
+
+        Texture2D _200x200;
     }
 }
