78c5862ad8ebd2ab85ebe8798bd3947f1c3cd316
Nikos Kastellanos <nkastellanos@gmail.com>
2025-10-04 13:09:10 +0300

    KNI Examples

---
+++ Example/Platforms/Example.WinFormsDX11/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GameProject
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // Uncomment this line to enable VR with the nkast.Kni.Platform.WinForms.DX11.OculusOVR package.
+            //Microsoft.Xna.Platform.XR.XRFactory.RegisterXRFactory(new Microsoft.Xna.Platform.XR.LibOVR.ConcreteXRFactory());
+
+            using (var game = new GameRoot())
+                game.Run();
+        }
+    }
+}
